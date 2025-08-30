0a9b5ba9bcd36e62668e84d5606f8e89239fc4b7
Abdelrahman Dawoud <abdelrahmandawoud11@gmail.com>
2025-08-31 01:24:39 +0300

    Real-SEcond

--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -3,7 +3,8 @@ function WelcomeMessage() {
     <div>
       <h1>Welcome to ALX React APP!</h1>
       <p>This is a simple JSX component.</p>
-      <p>"I am learning about JSX!"</p>
+      <p>I am learning about JSX!</p>
+      <p>Hello everyone, I am learning React at ALX!</p>
     </div>
   );
 }
