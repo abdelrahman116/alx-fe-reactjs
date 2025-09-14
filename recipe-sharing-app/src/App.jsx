c32fc154b6579e7fc5da164de599448149411b6d
Abdelrahman Dawoud <abdelrahmandawoud11@gmail.com>
2025-09-14 22:20:56 +0300

    First

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import AddRecipeForm from "./Components/AddRecipeForm";
+import RecipeList from "./Components/RecipeList";
+function App() {
+  return (
+    <div>
+      <RecipeList />
+      <AddRecipeForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ recipe-sharing-app/src/Components/RecipeList.jsx
@@ -0,0 +1,16 @@
+import { useRecipeStore } from "../recipeStore/recipeStore";
+
+export default function RecipeList() {
+  const recipes = useRecipeStore((state) => state.recipes);
+
+  return (
+    <>
+      {recipes.map((recipe) => (
+        <div key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
+    </>
+  );
+}
