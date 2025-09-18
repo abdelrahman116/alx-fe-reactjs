77f07eeff5a72e9db6e63aee225c400d381922b4
Abdelrahman Dawoud <112118879+abdelrahman116@users.noreply.github.com>
2025-09-18 18:24:12 +0300

    Update DeleteRecipeButton.jsx

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,16 +1,13 @@
 import { useRecipeStore } from "./recipeStore";
-import { useNavigate } from "react-router-dom";
 
 export default function DeleteRecipeButton() {
   const DeleteRecipe = useRecipeStore((state) => state.deleteRecipe);
   const recipes = useRecipeStore((state) => state.recipes);
   const lastRecipe = recipes[recipes.length - 1];
-  const navigate = useNavigate();
 
   const handleDelete = () => {
     if (lastRecipe && window.confirm("Delete the last recipe?")) {
       DeleteRecipe(lastRecipe.id);
-      navigate("/");
     }
   };
   return (
