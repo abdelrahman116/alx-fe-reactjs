bc66b1035b45229ca3a1228f56f8f0ce60f79cd0
Abdelrahman Dawoud <abdelrahmandawoud11@gmail.com>
2025-09-28 23:03:14 +0300

    First

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className=" mt-50 text-center text-red-500 text-xl">
+        I'm styled by tailwind
+      </p>
+    </>
+  );
+}
+
+export default App;
