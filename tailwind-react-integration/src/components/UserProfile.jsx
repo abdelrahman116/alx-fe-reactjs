cc0e6aac08bf8d1618503a5ac059b2b0a431ccaa
Abdelrahman Dawoud <abdelrahmandawoud11@gmail.com>
2025-09-29 00:35:24 +0300

    First

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,13 +1,13 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg ">
+    <div className="user-profile bg-gray-100 sm:p-4 sm:max-w-xs md:max-w-sm md:p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg ">
       <img
-        className="rounded-full w-36 h-36 mx-auto"
+        className="rounded-full md:w-36 md:h-36 sm:w-24 sm:h-24 mx-auto"
         src="https://via.placeholder.com/150"
         alt="User"
       />
-      <h1 className="text-xl  text-blue-800 my-4">John Doe</h1>
-      <p className="text-gray-600text-base">
+      <h1 className="md:text-xl sm:text-lg text-blue-800 my-4">John Doe</h1>
+      <p className="text-gray-600 md:text-base sm:text-sm">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
