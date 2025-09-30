49ab3a823b0515e2eecddaf2542ee23f9413972b
Abdelrahman Dawoud <abdelrahmandawoud11@gmail.com>
2025-10-01 01:31:38 +0300

    2

--- recipe-sharing-platform/src/components/NavBar.jsx
+++ recipe-sharing-platform/src/components/NavBar.jsx
@@ -1,18 +1,16 @@
 import { Link } from "react-router-dom";
 export default function NavBar() {
   return (
-    <nav className="top-0 h-7 text-center bg-green-300">
+    <nav className="top-0 h-12 text-center bg-green-300">
       <ul className="flex flex-row  justify-around gap-4 text-green-500 font-bold ">
         <Link to="/">
           {" "}
-          <li className="hover:text-green-500 hover:cursor-pointer h-7 rounded-t-lg  bg-white hover:scale-110 duration-200 ease-in px-2 ">
+          <li className="text-3xl hover:text-green-500 hover:cursor-pointer h-12 rounded-t-lg  bg-white hover:scale-110 duration-200 ease-in px-2 ">
             Home
           </li>
         </Link>
-        <li className="hover:text-green-500 hover:cursor-pointer rounded-t-lg bg-white hover:scale-110 duration-200 ease-in px-2 ">
-          Details
-        </li>
-        <li className="hover:text-green-500 hover:cursor-pointer rounded-t-lg bg-white hover:scale-110 duration-200 ease-in px-2 ">
+
+        <li className="text-3xl hover:text-green-500 hover:cursor-pointer rounded-t-lg bg-white hover:scale-110 duration-200 ease-in px-2 ">
           Create a Recipie
         </li>
       </ul>
--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -10,30 +10,42 @@ export default function RecipeDetail() {
   }
 
   return (
-    <div className="p-6 max-w-2xl mx-auto">
-      <h1 className="text-3xl font-bold mb-4">{recipe.title}</h1>
-      <img
-        src={recipe.image}
-        alt={recipe.title}
-        className="w-full h-64 object-cover rounded-lg mb-4"
-      />
-      <p className="mb-4">{recipe.summary}</p>
+    <div className=" bg-green-100 p-6 w-full mx-auto">
+      <div className="ml-100">
+        <h1 className="text-3xl font-bold mb-4">{recipe.title}</h1>
+        <img
+          src={recipe.image}
+          alt={recipe.title}
+          className="w-fit h-64 object-cover rounded-lg mb-4 "
+        />
+        <p className="mb-4">{recipe.summary}</p>
 
-      <h2 className="text-xl font-semibold mb-2">Requirements</h2>
-      <ul className="list-disc pl-6 mb-6">
-        {recipe.requirements?.map((req, i) => (
-          <li key={i}>{req}</li>
-        ))}
-      </ul>
+        <h2 className="text-xl font-semibold mb-6">Ingredients</h2>
+        <ul className="list-disc pl-6 mb-6">
+          {recipe.ingredients?.map((req, i) => (
+            <div>
+              <li className=" list-none mb-3" key={i}>
+                <span className="text-2xl rounded-4xl bg-green-300 text-white font-bold px-1.5 py-1">
+                  {i + 1}
+                </span>{" "}
+                <span className="font-bold"> {req}</span>
+              </li>
+            </div>
+          ))}
+        </ul>
 
-      <h2 className="text-xl font-semibold mb-2">Steps</h2>
-      <ol className="list-decimal pl-6">
-        {recipe.steps?.map((step, i) => (
-          <li key={i} className="mb-2">
-            {step}
-          </li>
-        ))}
-      </ol>
+        <h2 className="text-xl font-semibold mb-6">Instructions</h2>
+        <ol className="list-decimal pl-6">
+          {recipe.instructions?.map((step, i) => (
+            <li className=" list-none mb-3" key={i}>
+              <span className="text-2xl rounded-4xl bg-green-300 text-white font-bold px-1.5 py-1">
+                Step {i + 1}
+              </span>{" "}
+              <span className="font-bold"> {step}</span>
+            </li>
+          ))}
+        </ol>
+      </div>
     </div>
   );
 }
