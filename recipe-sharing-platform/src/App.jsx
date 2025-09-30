cf8429f1443b5848476dccb6b6c425d93bab843d
Abdelrahman Dawoud <abdelrahmandawoud11@gmail.com>
2025-09-30 21:56:05 +0300

    1

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./App.css";
 import HomePage from "./components/HomePage";
+import RecipeDetail from "./components/RecipeDetail";
 
 function App() {
   <style></style>;
   return (
     <body className="bg-green-50">
       <HomePage />
+      <RecipeDetail />
     </body>
   );
 }
