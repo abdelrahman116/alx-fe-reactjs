14138298c955ba1c2e36de3da8f6eedbb6336158
Abdelrahman Dawoud <abdelrahmandawoud11@gmail.com>
2025-10-19 01:36:39 +0300

    Forth Try

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,35 +1,13 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Link,
-  useRouteMatch,
-} from "react-router-dom";
-import ProfileDetails from "./ProfileDetails";
-
 export default function Profile() {
-  let { path, url } = useRouteMatch();
-
   return (
     <>
-      <div>
-        <h2>Dashboard</h2>
-        <ul>
-          <li>
-            <Link to={`${url}/profileDetails`}>Profile-Details</Link>
-          </li>
-        </ul>
-      </div>
+      <Routes>
+        <Route path="/" element={<h3>Please select an option.</h3>} />
+        <Route path="/profileDetails" element={<ProfileDetails />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
       <h1>Hi My name is: David</h1>
       <h3>22 Years Old</h3>
-      <Switch>
-        <Route exact path={path}>
-          <h3>Please select an option.</h3>
-        </Route>
-        <Route path={`${path}/profileDetails`}>
-          <ProfileDetails />
-        </Route>
-      </Switch>
     </>
   );
 }
