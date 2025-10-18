7433d5f5bf12265506950e26c481e8570fef0445
Abdelrahman Dawoud <abdelrahmandawoud11@gmail.com>
2025-10-19 01:38:09 +0300

    Forth Try

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,10 +1,12 @@
+import ProfileSettings from "./ProfileSettings";
+
 export default function Profile() {
   return (
     <>
       <Routes>
         <Route path="/" element={<h3>Please select an option.</h3>} />
         <Route path="/profileDetails" element={<ProfileDetails />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/profileSettings" element={<ProfileSettings />} />
       </Routes>
       <h1>Hi My name is: David</h1>
       <h3>22 Years Old</h3>
---
+++ react-router-advanced/src/components/ProfileSettings.jsx
@@ -0,0 +1,9 @@
+export default function ProfileSettings() {
+  return (
+    <>
+      <h1>Profie-Settings</h1>
+      <h2>Hi My name is: Moh</h2>
+      <h3>21 Years Old</h3>
+    </>
+  );
+}
