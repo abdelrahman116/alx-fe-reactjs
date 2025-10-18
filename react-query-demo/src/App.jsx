0a577f393f1bac4ee468f09058a67d4b773d3230
Abdelrahman Dawoud <abdelrahmandawoud11@gmail.com>
2025-10-19 01:17:28 +0300

    Almost Done

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
+import PostComponents from "./components/PostsComponent";
 
 const queryClient = new QueryClient();
 
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <PostsComponent />
+      <PostComponents />
     </QueryClientProvider>
   );
 }
--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,5 +1,4 @@
-import { useQuery } from "react-query";
-
+import { useQuery } from "@tanstack/react-query";
 // Define a fetch function that can be used to fetch data from an API
 const fetchData = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
@@ -8,19 +7,33 @@ const fetchData = async () => {
 
 const PostComponents = () => {
   // Use the useQuery hook to handle data fetching and caching
-  const { data, error, isLoading } = useQuery("fetchData", fetchData);
 
+  const { data, error, isLoading, refetch } = useQuery({
+    queryKey: ["fetchData"],
+    queryFn: fetchData,
+  });
   // Handle loading state
   if (isLoading) return <div>Loading...</div>;
   // Handle error state
   if (error) return <div>Error loading data</div>;
+  const handleRefetch = () => {
+    refetch();
+  };
+  data?.map((post) => ({
+    title: post.title,
+    body: post.body,
+    id: post.id,
+  }));
 
   // Render the fetched data
   return (
     <div>
       {data.map((item) => (
-        <div key={item.id}>{item.name}</div>
+        <div key={item.id}>
+          {item.title} {item.body}
+        </div>
       ))}
+      <button onClick={handleRefetch}>Refetch data</button>
     </div>
   );
 };
